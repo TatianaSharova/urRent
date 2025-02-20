bf0539eda3467a62b2b6ecc324e49216a635b30e
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-02-20 19:03:51 +0400

    add project settings

---
+++ src/config.py
@@ -0,0 +1,27 @@
+import os
+
+from pathlib import Path
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).parent.parent
+
+DB_PATH = BASE_DIR / 'db.sqlite3'
+
+class DbSettings(BaseModel):
+    '''Класс настроек для подключения к бд.'''
+    url: str = f'{os.getenv("DB_URL")}:///{DB_PATH}'
+    echo: bool = True
+
+
+class Settings(BaseSettings):
+    '''Класс настроек проекта.'''
+    api_v1_prefix: str = '/api/v1'
+
+    db: DbSettings = DbSettings()
+
+
+settings = Settings()

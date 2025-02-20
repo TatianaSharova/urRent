a8bcc151ef083a36491de42be615b06dc5bcb78f
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-02-20 19:04:51 +0400

    add db helper

--- src/db/db.py
+++ src/db/db.py
@@ -1,39 +1,66 @@
-import os
+from asyncio import current_task
 from typing import AsyncGenerator
 
-from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
-                                    create_async_engine)
-
-# from app.models import Model TODO: заменить
-
-load_dotenv()
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME = os.getenv('DB_NAME')
-
-# engine = create_async_engine(
-#     f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@postgres/{DB_NAME}'
-# )
-engine = create_async_engine(
-    'sqlite+aiosqlite:///db_sqlite3.db',
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
 )
 
-db_session = async_sessionmaker(engine, expire_on_commit=False)
+from config import settings
+from models import Base
 
+
+class DatabaseHelper:
+    '''Класс настроек движка бд.'''
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def scoped_session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
+        session = self.get_scoped_session()
+        yield session
+        await session.close()
+
+
+db_helper = DatabaseHelper(
+    url=settings.db.url,
+    echo=settings.db.echo,
+)
 
 async def create_table():
-    async with engine.begin() as conn:
-        await conn.run_sync(Model.metadata.create_all)
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
 async def delete_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Model.metadata.drop_all)
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with db_session() as session:
+    async with db_helper.session_factory() as session:
         try:
             yield session
         finally:

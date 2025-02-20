bf8397ff6082e4195ba1143e5a3e5462672c6641
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-02-20 17:28:19 +0400

    add main.py

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+import uvicorn
+
+from db.db import create_table, delete_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_table()
+    print('start')
+    yield
+    # await delete_tables()
+    print('end')
+
+app = FastAPI(lifespan=lifespan)
+
+
+# app.include_router(user_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)

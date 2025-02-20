4590a180e12c0fcbd0909c7f961dd59c75f7e992
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-02-20 19:05:33 +0400

    add base model class

---
+++ src/models/base.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+
+class Base(DeclarativeBase):
+    '''Базовая модель.'''
+    __abstract__ = True
+
+    # даём имя таблице на основе названия модели
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f'{cls.__name__.lower()}s'
+
+    id: Mapped[int] = mapped_column(primary_key=True)

bb58f711499de21600abdf853696a898fd35551e
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-04-04 14:54:12 +0400

    pep8

--- src/config.py
+++ src/config.py
@@ -11,10 +11,11 @@ BASE_DIR = Path(__file__).parent.parent
 
 DB_PATH = BASE_DIR / 'db.sqlite3'
 
+
 class DbSettings(BaseModel):
     '''Класс настроек для подключения к бд.'''
     url: str = f'{os.getenv("DB_URL")}:///{DB_PATH}'
-    echo: bool = True # для отладки
+    echo: bool = True  # для отладки
 
 
 class Settings(BaseSettings):

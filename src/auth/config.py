75fd7d99dded5c2fec5aa506ceca68b4615195ce
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-04-04 14:55:50 +0400

    fix private keys names

--- src/auth/config.py
+++ src/auth/config.py
@@ -2,14 +2,15 @@ from pathlib import Path
 from pydantic import BaseModel
 
 
-BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__).parent
 
 
 class JWTAuth(BaseModel):
     '''Класс настроек JWT-аунтефикации.'''
-    private_key_path: Path = BASE_DIR / 'certs' / 'jwt_private.pem'
-    public_key_path: Path = BASE_DIR / 'certs' / 'jwt_public.pem'
+    private_key_path: Path = BASE_DIR / 'certs' / 'jwt-private.pem'
+    public_key_path: Path = BASE_DIR / 'certs' / 'jwt-public.pem'
     algorithm: str = 'RS256'
+    access_token_expire_min: int = 15
 
 
 auth_settings = JWTAuth()

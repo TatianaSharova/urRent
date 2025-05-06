180acd845480f5b72e650ae01870ad4e15b68b03
Татьяна Шарова <tanyasharova2001@mail.ru>
2025-05-06 21:32:42 +0400

    feat/add refresh url

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,123 +1,50 @@
-from jwt.exceptions import InvalidTokenError
 from fastapi import (
     APIRouter,
-    Depends,
-    Form,
-    HTTPException,
-    status,
+    Depends
 )
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, OAuth2PasswordBearer
 
-from auth import utils as auth_utils
-from .schemas import UserAuth, TokenInfo
+from auth.schemas import UserAuth, TokenInfo
+from auth.helpers import create_access_token, create_refresh_token
+from auth.validation import (get_current_auth_user_for_refresh,
+                             validate_auth_user,
+                             get_current_active_auth_user,
+                             get_current_token_payload)
 
 oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="/api/v1/jwt/login/",
 )
 
-
-auth_jwt_router = APIRouter(prefix="/jwt", tags=["JWT"])
-
-john = UserAuth(
-    username="john",
-    password=auth_utils.hash_password("qwerty"),
-    email="john@example.com",
-)
-sam = UserAuth(
-    username="sam",
-    password=auth_utils.hash_password("secret"),
-)
-
-users_db: dict[str, UserAuth] = {
-    john.username: john,
-    sam.username: sam,
-}
+http_bearer = HTTPBearer(auto_error=False)
 
 
-def validate_auth_user(
-    username: str = Form(),
-    password: str = Form(),
-):
-    '''Проверяем существует ли пользователь и активен ли он.'''
-    if not (user := users_db.get(username)):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="invalid username or password",
-        )
-
-    if not auth_utils.validate_password(
-        password=password,
-        hashed_password=user.password,
-    ):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="invalid username or password",
-        )
-
-    if not user.active:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="user inactive",
-        )
-
-    return user
-
-
-def get_current_token_payload(
-    token: str = Depends(oauth2_scheme),
-) -> dict:
-    '''Проверяем токен пользователя.'''
-    try:
-        payload = auth_utils.decode_jwt(
-            token=token,
-        )
-    except InvalidTokenError as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"invalid token error: {e}"
-        )
-    return payload
-
-
-def get_current_auth_user(
-    payload: dict = Depends(get_current_token_payload),
-) -> UserAuth:
-    '''Получаем аутентифицированного юзера.'''
-    username: str | None = payload.get("sub")
-    if user := users_db.get(username):
-        return user
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="token invalid (user not found)",
-    )
-
-
-def get_current_active_auth_user(
-    user: UserAuth = Depends(get_current_auth_user),
-):
-    '''Получаем аутентифицированного активного юзера.'''
-    if user.active:
-        return user
-    raise HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail="user inactive",
-    )
+auth_jwt_router = APIRouter(prefix="/jwt", tags=["JWT"],
+                            dependencies=[Depends(http_bearer)])
 
 
 @auth_jwt_router.post("/login/", response_model=TokenInfo)
 def auth_user_issue_jwt(
     user: UserAuth = Depends(validate_auth_user),
 ):
-    '''Получение токена для юзера.'''
-    jwt_payload = {
-        "sub": user.username,
-        "username": user.username,
-        "email": user.email,
-    }
-    token = auth_utils.encode_jwt(jwt_payload)
+    access_token = create_access_token(user)
+    refresh_token = create_refresh_token(user)
     return TokenInfo(
-        access_token=token,
-        token_type="Bearer",
+        access_token=access_token,
+        refresh_token=refresh_token,
+    )
+
+
+@auth_jwt_router.post(
+    "/refresh/",
+    response_model=TokenInfo,
+    response_model_exclude_none=True,
+)
+def auth_refresh_jwt(
+    user: UserAuth = Depends(get_current_auth_user_for_refresh),
+):
+    access_token = create_access_token(user)
+    return TokenInfo(
+        access_token=access_token,
     )
 
 

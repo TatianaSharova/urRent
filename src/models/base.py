8332b99200357c8ad33798a76d9cebafad788934
Tatiana <kroxxatt@gmail.com>
2025-05-16 15:59:20 +0400

    feat/add metadata to BaseModel

--- src/models/base.py
+++ src/models/base.py
@@ -1,10 +1,16 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from config import settings
 
 
 class Base(DeclarativeBase):
     """Базовая модель."""
 
     __abstract__ = True
+
+    metadata = MetaData(
+        namiing_convention=settings.db.naming_convention,
+    )
 
     # даём имя таблице на основе названия модели
     @declared_attr.directive
